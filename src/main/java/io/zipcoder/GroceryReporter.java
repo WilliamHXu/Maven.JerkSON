788860411891f99006d0fba2998cfc7273c14094
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-03-18 16:08:59 -0400

    Finished lab

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,6 +1,12 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
 public class GroceryReporter {
     private final String originalFileText;
@@ -9,8 +15,84 @@ public class GroceryReporter {
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
 
+    public LinkedHashMap<String, LinkedHashMap> mapMaker(){
+        ItemParser itemParser = new ItemParser();
+        List<Item> itemList = itemParser.parseItemList(originalFileText);
+        LinkedHashMap<String, LinkedHashMap> result = new LinkedHashMap<>();
+        for(Item item : itemList){
+            boolean inserted = false;
+            String name = item.getName();
+            Set<String> itemNames = result.keySet();
+            for(String key : itemNames){
+                if(name.equals(key)){
+                    // Get in the map
+                    putInPriceMap(item, result.get(key));
+                    inserted = true;
+                }
+            }
+            if(!inserted){
+                LinkedHashMap<Double, Integer> priceMap = new LinkedHashMap<>();
+                priceMap.put(item.getPrice(), 1);
+                result.put(item.getName(), priceMap);
+            }
+        }
+        return result;
+    }
+
+    private void putInPriceMap(Item item, LinkedHashMap<Double, Integer> priceMap) {
+        boolean inserted = false;
+        Set<Double> prices = priceMap.keySet();
+        for (Double price : prices){
+            if (price.equals(item.getPrice())){
+                Integer count = priceMap.get(price);
+                priceMap.put(price, ++count);
+                inserted = true;
+            }
+        }
+        if(!inserted){
+            priceMap.put(item.getPrice(), 1);
+        }
+    }
+
     @Override
     public String toString() {
-        return null;
+        LinkedHashMap<String, LinkedHashMap> fullMap = mapMaker();
+        Set<String> itemNames = fullMap.keySet();
+        String result = "";
+        Integer numberOfEntries = 0;
+        for(String name : itemNames){
+            //result += makeItemString(name, fullMap.get(name));
+            Integer total = 0;
+            LinkedHashMap<Double, Integer> priceOccurrences = fullMap.get(name);
+            Set<Double> prices = priceOccurrences.keySet();
+            String priceString = "";
+            for (Double price : prices){
+                priceString += String.format("Price:%7.2f        seen: %d time%s", price, priceOccurrences.get(price), plural(priceOccurrences.get(price))) + "\n";
+                priceString += "-------------        -------------\n";
+                total += priceOccurrences.get(price);
+            }
+            result += String.format("name:%8s        seen: %d time%s", capitalize(name), total, plural(total)) + "\n" +
+                "=============        =============\n";
+            result += priceString + "\n";
+            numberOfEntries += total;
+        }
+        String[] temp = originalFileText.split("##");
+        Integer numberOfErrors = temp.length - numberOfEntries;
+        result += "Errors               seen: " + numberOfErrors + " times\n";
+        return result;
+    }
+
+    private String capitalize(String name) {
+        if (name.length() > 0){
+            return "" + ((Character) name.charAt(0)).toString().toUpperCase() + name.substring(1);
+        }
+        return "";
+    }
+
+    private String plural(Integer integer) {
+        if (integer > 1){
+            return "s";
+        }
+        return "";
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,75 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
-    public List<Item> parseItemList(String valueToParse) {
-        return null;
+    public List<Item> parseItemList(String valueToParse){
+        String[] items = valueToParse.split("##");
+        List<Item> result = new ArrayList<>();
+        for (String itemJSON: items) {
+            String matchString = itemJSON + "##";
+            try {
+                result.add(parseSingleItem(matchString));
+            }
+            catch (ItemParseException e){
+            }
+        }
+        return result;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        String regex = makeRegex();
+        // Pattern and Matcher
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(singleItem);
+        if (matcher.matches()) {
+            String name = matcher.group(2).toLowerCase();
+            // Trying to catch this silly co0kie;
+            if(name.equals("co0kies")){
+                name = "cookies";
+            }
+            Double price = Double.valueOf(matcher.group(4));
+            String type = matcher.group(6).toLowerCase();
+            String expiration = matcher.group(8).toLowerCase();
+            return new Item(name, price, type, expiration);
+        }
+        throw new ItemParseException();
+    }
+
+    private static String makeRegex() {
+        // Group
+//        1 : name
+        String nameRegex = "(\\w+)";
+//        2 : separator
+        String separator = "[:@^*%;!]";
+//        3 : name value
+        String nameValue = "(\\w+)";
+//        4 : price
+        String priceRegex = "(\\w+)";
+//        5 : separator
+        String separator2 = separator;
+//        6 : dollars
+        String dollars = "(\\d+\\.\\d{0,2})";
+//        7 : type
+        String typeRegex = "(\\w+)";
+//        8 : separator
+        String separator3 = separator;
+//        9 : type value
+        String typeValue = "(\\w+)";
+//        10: expiration
+        String expirationRegex = "(\\w+)";
+//        11: separator
+        String separator4 = separator;
+//        12: date
+        String date = "(\\d{1,2}/\\d{1,2}/\\d{4})";
+        // Build regex string
+        return nameRegex + separator + nameValue + separator +
+                priceRegex + separator2 + dollars + separator +
+                typeRegex + separator3 + typeValue + separator +
+                expirationRegex + separator4 + date + "##";
     }
 }
